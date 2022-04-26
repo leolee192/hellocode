f642a540a0b0256d74875f587901f4b615f505b6
leolee192 <leolee192@163.com>
2022-04-26 19:14:29 +0800

    Segment Tree (keep two versions)

--- RangeQuery.cpp
+++ RangeQuery.cpp
@@ -1,6 +1,60 @@
-/** segment tree */
+/** segment tree (point update) */
 template<class T>
 struct SegmentTree {
+  T (*Op)(T, T);
+  T Identity;
+
+  int n;
+  T *data;
+
+  // index [0, n_)
+  SegmentTree(int n_, T (*op_)(T, T), T identity_) {
+    Op = op_;
+    Identity = identity_;
+
+    // enlarge to power of 2 for simplicity
+    n = 1;
+    while (n < n_) n *= 2;
+
+    data = new T[2*n - 1];
+    for (int i=0; i< 2*n - 1; ++i) {
+      data[i] = Identity;
+    }
+  }
+
+  void update(int k, T a) {
+    k += n - 1; // leaf node
+    data[k] = a;
+    while (k > 0) {
+      k = (k - 1) / 2; // upwards
+      data[k] = Op(data[2*k + 1], data[2*k + 2]);
+    }
+  }
+
+  // RMQ from [a, b)
+  T query(int a, int b) {
+    return _query(a, b, 0, 0, n);
+  }
+
+  // sub-query in interval [l, r)
+  T _query(int a, int b, int k, int l, int r) {
+    if (r <= a || b <= l) { // [a,b) not interleave with [l,r)
+      return Identity;
+    }
+    if (a <= l && r <= b) { // [a, b) cover [l, r)
+      return data[k];
+    } else {
+      T vl = _query(a, b, 2*k + 1, l, (l+r) / 2);
+      T vr = _query(a, b, 2*k + 2, (l+r) / 2, r);
+      return Op(vl, vr);
+    }
+  }
+};
+/** END of segment tree (point update) */
+
+/** segment tree (interval update) */
+template<class T>
+struct SegmentTreeIntervalUpdate {
   T (*AggOp)(T, T);
   T AggIdentity;
 
@@ -12,7 +66,7 @@ struct SegmentTree {
   T *delta;
 
   // index [0, n_)
-  SegmentTree(int n_,
+  SegmentTreeIntervalUpdate(int n_,
               T (*aggOp_)(T, T), T aggIdentity_,
               T (*updateOp_)(T, T), T updateIdentity_, T (*multiUpdateOp_)(T, T, int)) {
     AggOp = aggOp_;
@@ -96,4 +150,4 @@ struct SegmentTree {
     }
   }
 };
-/** END of segment tree */
+/** END of segment tree (interval update)  */
